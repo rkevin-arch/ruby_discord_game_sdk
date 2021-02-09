420ef9ff60045fe0fb88cb98864a504a9f60129a
rkevin <rk@rkevin.dev>
2021-02-08 19:22:28 -0800

    rename to DiscordGameSDK (can actually see the user class in irb now!)

--- ext/ruby_discord_game_sdk/ruby_discord_game_sdk.c
+++ ext/ruby_discord_game_sdk/ruby_discord_game_sdk.c
@@ -1,9 +1,9 @@
 #include "ruby_discord_game_sdk.h"
 #include "user.h"
 
-VALUE rb_mRubyDiscordGameSDK;
+VALUE rb_mDiscordGameSDK;
 
 void Init_ruby_discord_game_sdk(void) {
-    rb_mRubyDiscordGameSDK = rb_define_module("RubyDiscordGameSDK");
-    rb_discord_init_user(rb_mRubyDiscordGameSDK);
+    rb_mDiscordGameSDK = rb_define_module("DiscordGameSDK");
+    rb_discord_init_user(rb_mDiscordGameSDK);
 }
