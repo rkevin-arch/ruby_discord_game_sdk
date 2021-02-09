1f599d48983f99e08385b9044db21f51d61a4c5c
rkevin <rk@rkevin.dev>
2021-02-09 00:40:36 -0800

    force the linker into linking, add init function

--- ext/ruby_discord_game_sdk/extconf.rb
+++ ext/ruby_discord_game_sdk/extconf.rb
@@ -2,10 +2,20 @@
 
 require "mkmf"
 
-HEADER_DIR = File.join(File.dirname(__FILE__), "../../third_party/include")
+THIRDPARTY_DIR = File.join(File.dirname(__FILE__), "../../third_party")
+HEADER_DIR = File.join(THIRDPARTY_DIR, "include")
+if RUBY_PLATFORM.start_with?("x86_64")
+  LIBRARY_DIR = File.join(THIRDPARTY_DIR, "lib/x86_64")
+elsif RUBY_PLATFORM.start_with?("i686")
+  LIBRARY_DIR = File.join(THIRDPARTY_DIR, "lib/x86")
+else
+  abort "Unrecognized architecture (not x86 or x86_64)"
+end
 
 if not find_header("discord_game_sdk.h", HEADER_DIR)
   abort "Cannot find the Discord Game SDK header"
 end
+
+$LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -l:discord_game_sdk.so'
 
 create_makefile("ruby_discord_game_sdk/ruby_discord_game_sdk")
--- ext/ruby_discord_game_sdk/ruby_discord_game_sdk.c
+++ ext/ruby_discord_game_sdk/ruby_discord_game_sdk.c
@@ -19,10 +19,27 @@ VALUE rb_oDiscordPendingCallbacks;
 
 struct DiscordSDK DiscordSDK;
 
+VALUE rb_discord_init(VALUE self, VALUE client_id, VALUE flags){
+    struct DiscordCreateParams params;
+    DiscordCreateParamsSetDefault(&params);
+    params.client_id = NUM2LL(client_id);
+    params.flags = NUM2LL(flags);
+    enum EDiscordResult result = DiscordCreate(DISCORD_VERSION, &params, &DiscordSDK.core);
+    if (result != DiscordResult_Ok) {
+        DiscordSDK.core = NULL;
+        return INT2NUM(result);
+    }
+    DiscordSDK.initialized = true;
+    return INT2NUM(result);
+}
+
 void Init_ruby_discord_game_sdk(void) {
     memset(&DiscordSDK, 0, sizeof(struct DiscordSDK));
     rb_mDiscordGameSDK = rb_define_module("DiscordGameSDK");
+    rb_define_method(rb_mDiscordGameSDK, "init", rb_discord_init, 2);
+
     rb_discord_init_user(rb_mDiscordGameSDK);
+
     rb_discord_init_application_manager(rb_mDiscordGameSDK);
     rb_discord_init_user_manager(rb_mDiscordGameSDK);
     rb_discord_init_image_manager(rb_mDiscordGameSDK);
