ddf8da8c9bdb9058ee0dfaa33b10aeb3b3e738ff
rkevin <rk@rkevin.dev>
2021-02-08 17:03:33 -0800

    add game sdk header when compiling in extconf

--- ext/ruby_discord_game_sdk/extconf.rb
+++ ext/ruby_discord_game_sdk/extconf.rb
@@ -2,4 +2,10 @@
 
 require "mkmf"
 
+HEADER_DIR = File.join(File.dirname(__FILE__), "../../third_party/include")
+
+if not find_header("discord_game_sdk.h", HEADER_DIR)
+  abort "Cannot find the Discord Game SDK header"
+end
+
 create_makefile("ruby_discord_game_sdk/ruby_discord_game_sdk")
