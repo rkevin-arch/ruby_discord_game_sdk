799f800f578e5b08c22d99982bfdfa27affdf795
Matthew Lyons <matthew@nowaffles.com>
2022-03-22 10:28:33 -0700

    Fix some stuff maybe?

--- ext/ruby_discord_game_sdk/user_manager.c
+++ ext/ruby_discord_game_sdk/user_manager.c
@@ -15,7 +15,7 @@ VALUE rb_discord_user_manager_get_current_user(VALUE self) {
     return rb_discord_user_from_struct(&current_user);
 }
 
-VALUE rb_discord_user_manager_get_user(VALUE self, VALUE user_id, VALUE reply) {
+VALUE rb_discord_user_manager_get_user(VALUE self, VALUE user_id) {
     CHECK_DISCORD_MGR_INITIALIZED(user);
     VALUE callback = rb_discord_get_callback_proc(2);
     DiscordSDK.user->get_user(
@@ -29,18 +29,18 @@ VALUE rb_discord_user_manager_get_user(VALUE self, VALUE user_id, VALUE reply) {
 
 VALUE rb_discord_user_manager_get_current_user_premium_type(VALUE self) { //segfault
     CHECK_DISCORD_MGR_INITIALIZED(user);
-    bool* result;
+    enum EDiscordPremiumType result;
     DiscordSDK.user->get_current_user_premium_type(
         DiscordSDK.user,
         &result
     );
 
-    return BOOL2RB(result);
+    return INT2NUM(result);
 }
 
 void rb_discord_init_user_manager(VALUE module) {
     rb_cDiscordUserManager = rb_define_module_under(module, "UserManager");
     EXPOSE_FUNCTION(rb_cDiscordUserManager, user_manager, get_current_user, 0);
-    EXPOSE_FUNCTION(rb_cDiscordUserManager, user_manager, get_user, 2);
+    EXPOSE_FUNCTION(rb_cDiscordUserManager, user_manager, get_user, 1);
     EXPOSE_FUNCTION(rb_cDiscordUserManager, user_manager, get_current_user_premium_type, 0);
 }
